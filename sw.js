16bd6cf83722ccbda122a6d8e02a968b975aeede
langea75-hash <lange.a75@gmail.com>
2026-08-28 18:32:24 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='psss-02',A=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
